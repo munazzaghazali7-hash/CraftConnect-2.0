3f4cd56410605fa70510b8ebc877239f4a4bd0fe
Almunazza <munazzaghazali7@gmail.com>
2025-11-23 10:41:37 +0530

    Fix vite.config.js: Remove duplicate export default

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default {
-  base: "./"
-};
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   server: {
     port: 5173, // optional, default 5173
